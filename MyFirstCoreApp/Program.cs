e2f788f0d93e08a1cb502914cfa1cc8f61445bd1
Куприян <kupriyan5397@mail.ru>
2021-03-25 02:14:40 +0700

    FirstCommit

---
+++ MyFirstCoreApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Представьтесь, пожалуйста!");
+            string str = Console.ReadLine();
+            Console.WriteLine("Привет, " + str + "!");
+            Console.Read();
+            Console.Clear();
+        }
+    }
+}
